99db83a8d48e75b51378eaaaf263c88331618f12
Nepenthes <galaxy0314@qq.com>
2024-02-25 12:33:32 +0800

    编译器

--- ChlSystem/src/Base/compiler/LinkCompile.java
+++ ChlSystem/src/Base/compiler/LinkCompile.java
@@ -1,5 +1,18 @@
 package Base.compiler;
 
-public class LinkCompile {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class LinkCompile {
+    java.util.ArrayList<String> collectedPaths;
+     public ArrayList<String> collect(String[] paths){
+         for (String s:paths) {
+             collectedPaths.add(s);
+         }
+         return collectedPaths;
+     }
+     public void compile(){
+
+     }
 }
